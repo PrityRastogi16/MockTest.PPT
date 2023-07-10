8ce4b291dbec1f8cbe7c6115755ed58f2051e23e
Prity Rastogi <128986644+PrityRastogi16@users.noreply.github.com>
2023-07-10 16:41:59 +0530

    Add files via upload

---
+++ Mock-Test3/script.js
@@ -0,0 +1,8 @@
+const box = document.getElementById('box');
+
+box.addEventListener("click" , function(){
+    box.style.backgroundColor = "yellow";
+    box.style.height = "600px";
+    box.style.width = "600px";
+
+});
